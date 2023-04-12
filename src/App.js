02b8e31aa670ba05a5248171a017c0b310d52ab5
amirshams84 <amir.shams84@gmail.com>
2023-04-12 12:44:51 -0400

    first_update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Project Portfolio Website
+          Hey what is up!
         </p>
         <a
           className="App-link"
